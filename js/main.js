6d723a2eaf936ec172f99432630077039f196303
Mahesha Gouli <goulitcs@gmail.com>
2025-06-20 22:37:50 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
