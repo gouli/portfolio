d9b704ddb1f0949bdda6e89566ac02d75e7a11bf
Mahesha Gouli <goulitcs@gmail.com>
2025-06-21 19:32:10 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 document.querySelectorAll('.nav-links a').forEach(link => {
   link.addEventListener('click', e => {
     e.preventDefault();
+
     const target = document.querySelector(link.getAttribute('href'));
     target.scrollIntoView({ behavior: 'smooth' });
+
+    // Close the mobile menu if open
+    const navCollapse = document.getElementById('nav-collapse');
+    const hamburgerToggle = document.getElementById('hamburger-toggle');
+    if (navCollapse.classList.contains('open')) {
+      navCollapse.classList.remove('open');
+    }
   });
 });
